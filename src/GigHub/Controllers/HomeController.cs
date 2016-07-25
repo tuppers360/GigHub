12e1969c06f58d7cc31f8d17c1a91d76507011cb
Gareth Tupman <tuppers360@live.co.uk>
2016-07-25 15:46:49 +0100

    Styling Index Gig Display

--- src/GigHub/Controllers/HomeController.cs
+++ src/GigHub/Controllers/HomeController.cs
@@ -19,6 +19,7 @@ namespace GigHub.Controllers
         {
             var upcomingGigs = _context.Gigs
                 .Include(g => g.Artist)
+                .Include(g => g.Genre)
                 .Where(g => g.DateTime > DateTime.Now);
             return View(upcomingGigs);
         }
